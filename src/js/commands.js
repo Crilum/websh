900e9d2f2721eebf97175453a6ebfd8ce7bb8af7
Crilum <91354257+Crilum@users.noreply.github.com>
2022-04-21 16:10:48 -0600

    Add a ton of Search engines, as well as a help

--- src/js/commands.js
+++ src/js/commands.js
@@ -3,6 +3,7 @@ g = google;
 function google(args) {
     if (args != undefined) {
         search = args.replace(" ", "+")
+	search = args.replace("+", "%2B")
         window.open("https://www.google.com/search?q=" + search);
     } else {
         window.open("https://www.google.com");
@@ -24,12 +25,41 @@ function clear(args) {
 
 // Search on DuckDuckGo
 ddg = duckduckgo;
+s = duckduckgo;
+d = duckduckgo;
+search = duckduckgo;
 function duckduckgo(args) {
     if (args != undefined) {
         search = args.replace(" ", "+")
+	search = args.replace("+", "%2B")
         window.open("https://duckduckgo.com/?q=" + search);
     } else {
         window.open("https://duckduckgo.com/");
+    }
+}
+
+// Search on Wikipedia
+w = wikipedia;
+wiki = wikipedia;
+function wikipedia(args) {
+    if (args != undefined) {
+        search = args.replace(" ", "+")
+	search = args.replace("+", "%2B")
+        window.open("https://wikipedia.org/w/index.php?search=" + search);
+    } else {
+        window.open("https://wikipedia.org/");
+    }
+}
+
+// Search on Github
+gh = github;
+function github(args) {
+    if (args != undefined) {
+        search = args.replace(" ", "+")
+        search = args.replace("+", "%2B")
+        window.open("https://github.com/search?q=" + search);
+    } else {
+        window.open("https://github.com/");
     }
 }
 
@@ -38,3 +68,6 @@ function echo(args) {
     block_log(args);
 }
 
+function help() {
+    block_log("websh: the modern webshell emulator" + "<br/><br/>websh tools:<br/><br/>echo - print specified text<br/>clock - print time" + "<br/><br/>Search engines:<br/><br/>g, or google - go to https://google.com, or search for ARGS" + "<br/>" + "s, search, d, ddg, or duckduckgo - go to https://duckduckgo.com, or search for ARGS" + "<br/>" + "w, or wikipedia - go to https://wikipedia.org, or search for ARGS" + "<br/>" + "gh, or github - go to https://github.com, or search for ARGS" + "<br/>" + "")
+}
